54a7d2076bcd83f2830a02629aa7f8bc370ed927
Emergin <ngangaephantus112@gmail.com>
2022-04-08 11:08:57 -0700

    my

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
- * 
+ *
  * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
  * Return: Always 0 (Success)
