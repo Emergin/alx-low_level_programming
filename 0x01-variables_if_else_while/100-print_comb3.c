40e7fe6a424605240dba27622360ea66817ae248
Emergin <ngangaephantus112@gmail.com>
2022-04-08 11:59:26 -0700

    my

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,25 +5,25 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{
-  int tens;
-  int ones;
-
-  for (tens = 0; tens <= 9; tens++)
-    {
-      for (ones = tens + 1; ones <= 9; ones++)
 	{
-	  putchar(tens + '0');
-	  putchar(ones + '0');
+	int tens;
+	int ones;
 
-	  if (tens < 8)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
+	for (tens = 0; tens <= 9; tens++)
+	{
+	for (ones = tens + 1; ones <= 9; ones++)
+	{
+	putchar(tens + '0');
+	putchar(ones + '0');
+
+	if (tens < 8)
+	{
+	putchar(',');
+	putchar(' ');
 	}
-    }
-  putchar('\n');
+	}
+	}
+	putchar('\n');
 
-  return (0);
+return (0);
 }
