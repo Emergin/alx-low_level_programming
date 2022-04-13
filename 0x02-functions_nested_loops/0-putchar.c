058dfefaac0ee51681ea443fc61710a41e4ae6f9
Emergin <ngangaephantus112@gmail.com>
2022-04-13 10:04:16 -0700

    my

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
-#include "main.h"
-
+#include "trubel.h"
 /**
- * main - Prints _putchar as a message.
+ * main - print the word "trubel"
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i;
+	charp[] = "Trubel";
 
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
+	for(i=0; i<9; i++)
 	{
-		_putchar(str[count]);
+		_putchar(p[i]);
 	}
 	_putchar('\n');
 	return (0);
