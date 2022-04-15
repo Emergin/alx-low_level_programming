2d7bd7bcea6545171d9d8b715e53ec72bd5d33a1
Emergin <ngangaephantus112@gmail.com>
2022-04-14 23:54:06 -0700

    my

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * prints - numbers from 0 to 9
+ *
+ * followed by a new line
+ * Returns: 0.
+ */
+void print_numbers(int a)
+{
+	for(a = 0;a <= 9;a++)
+		_putchar(a);
+_putchar('\n');
+}
