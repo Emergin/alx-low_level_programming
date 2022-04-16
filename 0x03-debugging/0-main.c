d8304d36515aaaec0843046a0173a0c874e6449f
Emergin <ngangaephantus112@gmail.com>
2022-04-15 23:03:35 -0700

    my

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests function prints if the integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	positive _or_negative(1);
+
+	return(0);
+}
